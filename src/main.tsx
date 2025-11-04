a2ce1326155355c1abe9d5a7fe034b37b18337ae
MohammadShadid <mohammadshadid03@gmail.com>
2025-11-04 17:40:25 +0200

    refactor: remove App component and use inline Home element in routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
 import './index.css';
 
 
@@ -9,8 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-        </Route>
+        <Route path="/" element={<div>Home</div>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
