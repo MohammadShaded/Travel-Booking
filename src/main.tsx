af57c4d3abcf1e78b29b0549735254950b70df05
MohammadShadid <mohammadshadid03@gmail.com>
2025-11-05 08:52:10 +0200

    feat: implement global styles and smooth scrolling in CSS; update main route content

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from '@/pages/LoginPage';
 import './index.css';
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<div>this issssssssss home paaaaaaage</div>} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
