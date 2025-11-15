52278df9a236462d4c9807e5df7ffeeaaacf6a50
MohammadShadid <mohammadshadid03@gmail.com>
2025-11-15 12:25:15 +0200

    feat: implement Home page layout with Hero, FeaturedDeals, RecentlyVisited, and TrendingDestinations components

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,27 @@
+import Hero from './components/Hero';
+import FeaturedDeals from './components/FeaturedDeals';
+import RecentlyVisited from './components/RecentlyVisited';
+import TrendingDestinations from './components/TrendingDestinations';
+import styles from './Home.module.css';
+import { useScroll } from '@/hooks/useScroll';
+
 export default function Home() {
-    return <div>This is Home Page</div>;
+  const { isScrolled } = useScroll(80); 
+
+  return (
+    <div className={styles.home}>
+      <Hero isScrolled={isScrolled} />
+      
+      {/* Content sections */}
+      <div className={styles.contentSections + (isScrolled ? ' ' + styles.scrolledContentSections : '')}>
+        <FeaturedDeals isScrolled={isScrolled}  />
+        
+        {/* Recently Visited Hotels (only shown if user is logged in) */}
+        <RecentlyVisited />
+        
+        {/* Trending Destinations */}
+        <TrendingDestinations />
+      </div>
+    </div>
+  );
 }
